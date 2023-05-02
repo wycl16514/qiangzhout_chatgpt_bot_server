bfca7c5f08a80b4b3b57f8efec3818a4d0d78cf3
MAC <my@MACdeAir.lan>
2023-05-02 17:38:28 +0800

    set up server

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+import dotenv from "dotenv"
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet())
+
+
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+    console.log(`Example app listening at http localhsot:${PORT}`);
+})
