c9ec021b301f7712b8903232b13ed56b1f7dced0
MAC <my@MACdeAir.lan>
2023-05-02 17:50:45 +0800

    add cors

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
