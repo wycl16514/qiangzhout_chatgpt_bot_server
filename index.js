6acdbd093f38aa7eda49b8aa01af7e01755c5397
MAC <my@MACdeAir.lan>
2023-05-03 17:03:33 +0800

    add chatgpt api call

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ import cors from "cors"
 import dotenv from "dotenv"
 import helmet from "helmet";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js"
+
+const configuration = new Configuration(
+    {
+        apiKey: process.env.OPENAI_API_KEY,
+    }
+);
+export const openai = new OpenAIApi(configuration);
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet())
 
 
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -16,6 +24,8 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(helmet())
+app.use('/openai', openAiRoutes);
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
